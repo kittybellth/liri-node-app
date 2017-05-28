815c35f512fe70f52b43399329d194cafeac879c
kittybellth <kittybell.th@gmail.com>
2017-05-28 01:50:45 -0500

    finished

--- liri.js
+++ liri.js
@@ -6,53 +6,68 @@ var title = "";
 var Twitter = require('twitter');
 var spotify = require('spotify');
 var fs = require("fs");
-var Twit = new Twitter(config.twitterKeys);
+
 
 function initApp(){
-if (nodeArg[2]){
-    if (nodeArg[2] === "my-tweets"){
-        myTweeter();
-        return;
-    } else if (nodeArg[2] === "spotify-this-song"){
-        console.log("Welcome to Spotify!!")
-        if(nodeArg[3]){
-            for(i = 3; i<nodeArg.length; i++){
-                argArr.push(nodeArg[i]);
-            };
-            title = argArr.join(" ");
-            console.log("Song name : "+title);
-            mySpotify(0);
+    if (nodeArg[2]){
+        if (nodeArg[2] === "my-tweets"){
+            myTweeter();
+            fs.appendFile("log.txt", nodeArg[2]+"\n", function(err) {});
             return;
-        } else if(nodeArg[3] === undefined){
-            title = "The Sign";
-            console.log("Song name : "+title);
-            mySpotify(4);
+        } else if (nodeArg[2] === "spotify-this-song"){
+            console.log("");
+            console.log("Welcome to Spotify!!");
+            if(nodeArg[3]){
+                for(i = 3; i<nodeArg.length; i++){
+                    argArr.push(nodeArg[i]);
+                };
+                title = argArr.join(" ");
+                console.log("Song name : "+title);
+                mySpotify(0);
+                fs.appendFile("log.txt", nodeArg[2]+", "+title+"\n", function(err) {});
+                return;
+            } else if(nodeArg[3] === undefined){
+                title = "The Sign";
+                console.log("Song name : "+title);
+                mySpotify(4);
+                fs.appendFile("log.txt", nodeArg[2]+"\n", function(err) {});
+                return;
+            };
+            return;
+        } else if (nodeArg[2] === "movie-this") {
+            console.log("");
+            console.log("");
+            console.log("");
+            console.log("Sorry this function is not available");
+            console.log("");
+            console.log("");
+            console.log("");
+            return;
+        } else if (nodeArg[2] === "do-what-it-says") {
+            random();
+            fs.appendFile("log.txt", nodeArg[2]+"\n", function(err) {});
             return;
         };
-        return;
-    } else if (nodeArg[2] === "movie-this") {
-        
-        return;
-    } else if (nodeArg[2] === "do-what-it-says") {
-        random();
-        return;
     };
-};
 };
 
 initApp();
 
 function myTweeter(){
+
+    var Twit = new Twitter(config.twitterKeys);
+
     Twit.get('statuses/user_timeline',{count: 10},function(err, tweets, res) {
         if(err) throw err;
-        console.log("**********************************");
+        console.log("");
         console.log("Account name: "+tweets[0].user.name);
         console.log("**********************************");
         for(i = 0; i < tweets.length; i++){
             console.log("");
             console.log("Date: "+tweets[i].created_at);
             console.log("Tweet: "+tweets[i].text); 
-            console.log("------------------------------------------------");
+            console.log("");
+            console.log("----------------------------------------------------");
         };
         return;
     });
@@ -70,7 +85,8 @@ function mySpotify(x) {
             console.log("Song name: "+data.tracks.items[x].name);
             console.log("Preview link: "+data.tracks.items[x].preview_url);
             console.log("album: "+data.tracks.items[x].album.name);
-            console.log("-----------------------------------------------------------");
+            console.log("");
+            console.log("---------------------------------------------------");
         return;
     });
 };
